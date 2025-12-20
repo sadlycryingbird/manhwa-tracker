36ad4fa533ef23033ae5f8aadf1772617bcbed71
Khizr Fazal <197388844+sadlycryingbird@users.noreply.github.com>
2025-12-20 14:39:03 +0000

    Initial backend setup

---
+++ manhwa-tracker-backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.json(
+        {
+            manhwa: "solo leveling",
+            rating: "9.5"
+        }
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
