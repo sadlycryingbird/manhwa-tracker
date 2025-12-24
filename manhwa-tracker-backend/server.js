8bd3f48a0dad6e3ba0a50575d8ba359b9aa8c599
Khizr Fazal <197388844+sadlycryingbird@users.noreply.github.com>
2025-12-24 14:37:34 +0000

    added validation to requests, and better responses

--- manhwa-tracker-backend/server.js
+++ manhwa-tracker-backend/server.js
@@ -6,22 +6,34 @@ const manhwas = [];
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/manhwas", (req, res) => {
 
-    res.json(
-        {
-            manhwa: "solo leveling",
-            rating: "9.5"
-        }
-    );
-
+    res.status(200).json({
+        success: true,
+        count: manhwas.length,
+        data: manhwas
+    });
 });
 
 app.post("/manhwas", (req, res) => {
 
     const {title, rating, status} = req.body;
+
+    if (!title || !status) {
+        return res.status(400).json({
+            success: false,
+            message: "Title and status are required"
+        });
+    }
+
+    const newManhwa = {title, rating, status};
     manhwas.push({title, rating, status});
-    res.json({ success: true, manhwas, message: "Manhwa added"});
+
+    res.status(201).json({
+        success: true, 
+        newManhwa, 
+        message: "Manhwa added"
+    });
 
 });
 
@@ -29,10 +41,17 @@ app.delete("/manhwas/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
     if (!manhwas[id]) {
-        return res.status(404).json({success: false, message: "Manhwa not found"});
+        return res.status(404).json({
+            success: false, 
+            message: "Manhwa not found"
+        });
     }
     manhwas.splice(id, 1);
-    res.json({success: true, manhwas, message: "Manhwa deleted"});
+    res.json({
+        success: true, 
+        manhwas, 
+        message: "Manhwa deleted"
+    });
 });
 
 app.patch("/manhwas/:id", (req, res) => {
@@ -40,12 +59,25 @@ app.patch("/manhwas/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const status = req.body.status;
 
+    if (!status) {
+        return res.status(400).json({
+            success: false,
+            message: "status is required as input"
+        });
+    }
+
     if (!manhwas[id]) {
-        return res.status(404).json({ success: false, message: "Manhwa not found" });
+        return res.status(404).json({
+            success: false, 
+            message: "Manhwa not found"
+        });
     }
 
     manhwas[id].status = status;
-    res.json({ success: true, manhwas, message: "Manhwa updated"});
+    res.json({ 
+        success: true, 
+        manhwas, 
+        message: "Manhwa updated"});
 
 });
 
