0033aa1f6086a3fc5d768619cd730d8b946277c6
Khizr Fazal <197388844+sadlycryingbird@users.noreply.github.com>
2026-01-02 17:57:01 +0000

    Migrated User model to Typescript

--- manhwa-tracker-backend/src/models/User.ts
+++ manhwa-tracker-backend/src/models/User.ts
@@ -1,35 +1,35 @@
-import mongoose from "mongoose";
 import bcrypt from "bcryptjs"; // for password hashing
+import mongoose, { Document, Schema, Model, Types } from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true, 
-      lowercase: true, 
-      trim: true, // removes spaces
+export interface IUser extends Document {
+  email: string;  
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema : Schema<IUser> = new Schema(
+    {
+        email: { type: String, required: true, trim: true, lowercase: true, unique: true, },
+        password: { type: String, required: true, minlength: 6 }
     },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-  },
-  { timestamps: true } // adds createdAt and updatedAt
+    { timestamps : true }
 );
 
-// Pre-save hook to hash password before saving
-userSchema.pre("save", async function () {
-  // `this` is the document
+
+userSchema.pre<IUser>("save", async function () {
   if (!this.isModified("password")) return;
+
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-userSchema.methods.comparePassword = async function (candidatePassword) {
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User : Model<IUser> = mongoose.model<IUser>("User", userSchema);
+
 export default User;
